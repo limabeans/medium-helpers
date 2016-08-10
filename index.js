8031d1195ae9c38c152dd3d35d6ee4c8fcc4edc1
Angel Lim <toomanytypos@gmail.com>
2016-08-10 12:11:46 -0700

    search function will callback

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
-var https = require('https')
-var url = 'https://medium.com/search?q=javascript';
-
+var https = require('https');
 
 module.exports = {
-    print: function(x) {
-        console.log(x);
+    search: function(query, callback) {
+        var url = 'https://medium.com/search?q=' + query;
+        https.get(url, (res) => {
+            var output = '';
+            res.on('data', (d) => output+=d.toString())
+            res.on('end', () => {
+                var ind = output.indexOf('{"posts"')
+                var posts = output.substring(ind)
+                ind = posts.indexOf('}})') + 2
+                posts = posts.substring(0,ind)
+                var obj = JSON.parse(posts)
+                var posts_list = obj['posts']
+                function build_url(x) {
+                    var medium = 'https://medium.com'
+                    if (!x.homeCollection) {
+                        return medium + '/' + x.creator.username + '/' + x.uniqueSlug;
+                    } else {
+                        return medium + '/' + x.homeCollection.slug +'/' + x.uniqueSlug;
+                    }
+                }
+                var results = posts_list.map(function(x) {
+                    return {
+                        'title': x.title,
+                        'author': x.creator.name,
+                        'url': build_url(x)
+                    };
+                });
+                callback(results);
+            });
+        })        
     }
-}
-
-
-// https.get(url, (res) => {
-//     var output = '';
-//     res.on('data', (d) => output+=d.toString())
-//     res.on('end', () => {
-//         var ind = output.indexOf('{"posts"')
-//         var posts = output.substring(ind)
-//         ind = posts.indexOf('}})') + 2
-//         posts = posts.substring(0,ind)
-//         var obj = JSON.parse(posts)
-//         var posts_list = obj['posts']
-//         function build_url(x) {
-//             var medium = 'https://medium.com'
-//             if (!x.homeCollection) {
-//                 return medium + '/' + x.creator.username + '/' + x.uniqueSlug;
-//             } else {
-//                 return medium + '/' + x.homeCollection.slug +'/' + x.uniqueSlug;
-//             }
-//         }
-//         var results = posts_list.map(function(x) {
-//             return {
-//                 'title': x.title,
-//                 'author': x.creator.name,
-//                 'url': build_url(x)
-//             };
-//         });
-//         console.log(results)
-//     });
-// })
+};
