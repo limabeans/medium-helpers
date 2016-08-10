457165a36ed8a86686d274dca4067daca0977e2f
Angel Lim <toomanytypos@gmail.com>
2016-08-10 12:01:26 -0700

    test print statement

---
+++ index.js
@@ -0,0 +1,39 @@
+var https = require('https')
+var url = 'https://medium.com/search?q=javascript';
+
+
+module.exports = {
+    print: function(x) {
+        console.log(x);
+    }
+}
+
+
+// https.get(url, (res) => {
+//     var output = '';
+//     res.on('data', (d) => output+=d.toString())
+//     res.on('end', () => {
+//         var ind = output.indexOf('{"posts"')
+//         var posts = output.substring(ind)
+//         ind = posts.indexOf('}})') + 2
+//         posts = posts.substring(0,ind)
+//         var obj = JSON.parse(posts)
+//         var posts_list = obj['posts']
+//         function build_url(x) {
+//             var medium = 'https://medium.com'
+//             if (!x.homeCollection) {
+//                 return medium + '/' + x.creator.username + '/' + x.uniqueSlug;
+//             } else {
+//                 return medium + '/' + x.homeCollection.slug +'/' + x.uniqueSlug;
+//             }
+//         }
+//         var results = posts_list.map(function(x) {
+//             return {
+//                 'title': x.title,
+//                 'author': x.creator.name,
+//                 'url': build_url(x)
+//             };
+//         });
+//         console.log(results)
+//     });
+// })
